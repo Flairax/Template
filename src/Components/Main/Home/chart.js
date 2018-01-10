ae193a6bf0307ef95018267e025074d233ee3894
Golubev Andrew <golubevandreyua@gmail.com>
2018-01-10 14:36:23 +0200

    Fixed - chart, ratings, modals

--- src/Components/Main/Home/chart.js
+++ src/Components/Main/Home/chart.js
@@ -1,16 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import C3Chart from 'react-c3js';
-import 'c3/c3.css';
- 
-const data = {
+export const data = {
   columns: [
-    ['data1', 30, 200, 100, 400, 150, 250],
-    ['data2', 50, 20, 10, 40, 15, 25]
-  ]
+    ['Crelo', 30, 200, 100, 400, 150, 250],
+    ['Arlo', 250, 30, 60, 320, 25, 35],
+    ['Lokedo', 50, 20, 10, 40, 15, 25],
+    ['Bernali', 250, 30, 60, 320, 25, 35],
+    ['Axorfvido', 250, 30, 60, 320, 25, 35],
+    ['Menaxjeri', 250, 30, 60, 320, 25, 35],          
+ ]
 };
- 
-const mountNode = document.getElementById('react-c3js');
- 
-ReactDOM.render(<C3Chart data={data} />, mountNode);
+
+export const tooltip = {
+  grouped: false
+}
+
+let prevItemId = null;
+export const legend = {
+  item: {
+     onclick: function(id){
+       let temp = null;
+       let legend_items = Array.from(document.getElementsByClassName('c3-legend-item'))
+         .filter(item => {
+           return !item.classList.contains(`c3-legend-item-${id}`);
+         });
+
+       /**Remove effect of the selection from line and legend */
+       if (prevItemId === id) {
+         legend_items.forEach(item => {
+           item.classList.remove('shadow-legend');
+         });
+         document.getElementsByClassName(`c3-target-${id}`)[0]
+           .classList.remove('bolder-line');
+         prevItemId = null;
+
+         return null;
+       } else {
+         /**Making selected legend node visible */
+         document.getElementsByClassName(`c3-legend-item-${id}`)[0]
+           .classList.remove('shadow-legend');
+
+         /**Making all others legend nodes hidden*/
+         legend_items.forEach(item => {
+           item.classList.add('shadow-legend');
+         });
+
+         prevItemId = id;
+
+         /**Finding line which we need to make bolder*/
+         for (let i = 0; i < this.data.targets.length; i++) {
+           if (this.data.targets[i].id === id) {
+             temp = this.data.targets[i];
+             break;
+           }
+         }
+
+         this.api.unload(id);
+
+         /**Creating temp value to convert unloaded data to
+          * acceptable format for load*/
+         let tempArr = [];
+         for (let i = 0; i < temp.values.length; i++) {
+           tempArr.push(temp.values[i].value);
+
+         }
+         tempArr.unshift(temp.id);
+
+         /**Loading line to the end of the chart for
+          * display on top of all others. Also adding class
+          * to make it bolder*/
+         setTimeout(() => {
+           this.api.load({
+             columns: [
+               tempArr,
+             ],
+           });
+
+           Array.from(document.querySelectorAll(`.c3-target-${id}`)).forEach(element => {
+             element.classList.add('bolder-line');
+           });
+
+           /**Redrawing chart after it has been updated
+            * because of x,y markers corruption*/
+           setTimeout(() => {
+             this.api.flush();
+           }, 400);
+         }, 300);
+       }             
+     },
+     onmouseover: (id) => { },
+     onmouseout: (id) => { }
+  }
+}
--- src/Components/Main/Home/home.jsx
+++ src/Components/Main/Home/home.jsx
@@ -1,97 +1,10 @@
 import React, { Component } from 'react';
 
 import C3Chart from 'react-c3js';
+import { data, legend, tooltip } from './chart';
 import 'c3/c3.css';
 
-import $ from 'jquery';
-
-
-let prevItemId = null,
-   temp = null;
 export default class Home extends Component {
-   constructor(props) {
-      super(props);
-
-      this.data = {
-         columns: [
-            ['Crelo', 30, 200, 100, 400, 150, 250],
-            ['Arlo', 250, 30, 60, 320, 25, 35],
-            ['Lokedo', 50, 20, 10, 40, 15, 25],
-            ['Bernali', 250, 30, 60, 320, 25, 35],
-            ['Axorfvido', 250, 30, 60, 320, 25, 35],
-            ['Menaxjeri', 250, 30, 60, 320, 25, 35],          
-         ],
-         
-      };
-
-      this.tooltip = {
-         grouped: false
-       }
-
-      this.legend = {
-         item: {
-            onclick: function(id){
-               let legend_items = $(Array.from(document.getElementsByClassName('c3-legend-item'))
-                  .filter(item => {
-                     return !item.classList.contains(`c3-legend-item-${id}`);
-                  })
-               );
-
-               if (prevItemId === id) {
-                  legend_items.removeClass("shadow-legend");
-                  $(`.c3-target-${id}`).removeClass("bolder-line");
-                  prevItemId = null;
-
-                  return null;
-               } else {
-                  console.log(id)
-
-                  $(`.c3-legend-item-${id}`).removeClass("shadow-legend");
-
-                  legend_items.addClass("shadow-legend");
-                  
-                  prevItemId = id;
-
-                  for (let i = 0; i < this.data.targets.length; i++) {
-                     if(this.data.targets[i].id === id){
-                        temp = this.data.targets[i];
-                     }
-                     
-                  }
-
-                  this.api.unload(id);
-   
-                  let tempArr = [];
-                  for (let i = 0; i < temp.values.length; i++) {
-                     tempArr.push(temp.values[i].value)
-                     
-                  }
-                  tempArr.unshift(temp.id)
-                  console.log(temp.id)
-                  setTimeout(()=>{
-                     this.api.load({
-                        columns: [
-                           tempArr,                                           
-                        ],
-                     });
-                     $(`.c3-target-${id}`).addClass("bolder-line");
-                     setTimeout(() => {
-                        this.api.flush();
-                     }, 500)
-                  },300)                   
-               }
-            },
-            onmouseover: (id) => { },
-            onmouseout: (id) => { }
-         }
-      }
-   }
-
-   componentDidMount() {
-      $(".c3-chart-line").removeAttr("style");
-      $('.c3-legend-item').removeAttr("style");
-   }
-
    render() {
       return (
          <article id="Home">
@@ -113,14 +26,9 @@ export default class Home extends Component {
                   <li>Website is adaptable, try to change window size.</li>
                   <li><a href="https://github.com/Flairax/Template"> GitHub project</a></li>
                </ul>
-               <C3Chart 
-                  data={this.data} 
-                  legend={this.legend} 
-                  tooltip = {this.tooltip}
-               />
+               <C3Chart  data={data} legend={legend} tooltip = {tooltip} />
             </section>
          </article>
       );
    }
-
 }
